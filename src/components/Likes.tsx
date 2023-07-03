5484546ec458e57f54fc092ea2bcbd5db1a179bc
default-sameer <sameerjoshicr7@gmail.com>
2023-07-03 13:52:40 +0545

    Unique Property removed from Name in User Model

--- src/components/Likes.tsx
+++ src/components/Likes.tsx
@@ -1,110 +1,118 @@
 import axios from 'axios'
 import React from 'react'
-import {RiHeartAddLine, RiHeartFill} from 'react-icons/ri'
+import { RiHeartAddLine, RiHeartFill } from 'react-icons/ri'
 import { BASE_URL } from '../utils/config'
 import { useSession } from 'next-auth/react'
 import toast from 'react-hot-toast'
 
-const Likes = ({likes, setLikes, id, user, meme}) => {
-    // console.log(meme.id, id, user.name);
-    
-    const [liked, setLiked] = React.useState(false)
+const Likes = ({ likes, setLikes, id, user, meme }) => {
+  // console.log(meme.id, id, user.name);
 
-    const {data: session} = useSession()
+  const [liked, setLiked] = React.useState(false)
 
-    async function handleLike(){
-        if(!session){
-            toast.error('Please login to like Memes')
-            return
-        }
-        
-        if(!liked){
-            const updatedLikes = likes + 1
-            setLiked(true)
-            setLikes(updatedLikes)
-            try{
-                toast('Liked', {icon: '🧡'});
-                const body = {id, updatedLikes}
-                // await axios.post(`${BASE_URL}/api/meme/like`, {
-                //     headers: { 'Content-Type': 'application/json' },
-                //     body: JSON.stringify(body)
-                // })
-                await fetch(`${BASE_URL}/api/meme/like`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body),
-                })
-            }catch(err){
-                console.error(err)
-            }
-        }
-        if(liked){
-            const updatedLikes = likes - 1
-            setLiked(false)
-            setLikes(updatedLikes)
-            try{
-                toast('UnLiked', {icon: '💔'});
-                const body = {id, updatedLikes}
-                await fetch(`${BASE_URL}/api/meme/like`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body),
-                })
-            }catch(err){
-                console.error(err)
-            }
-        }
+  const { data: session } = useSession()
+
+  async function handleLike() {
+    if (!session) {
+      toast.error('Please login to like Memes')
+      return
     }
 
-    async function handleLikeandUnlike(){
-        if(!session){
-            toast.error('Please login to like Memes')
-            return
-        }
-        if(liked){
-            const updatedLikes = likes - 1
-            setLiked(false)
-            setLikes(updatedLikes)
-            try{
-                toast('UnLiked', {icon: '💔',});
-                const body = {id, updatedLikes, user}
-                await fetch(`${BASE_URL}/api/meme/likeandunlike`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body),
-                })
-            }catch(err){
-                console.error(err)
-            }
-        }
-        if(!liked){
-            const updatedLikes = likes + 1
-            setLiked(true)
-            setLikes(updatedLikes)
-            try{
-                toast('Liked', {icon: '🧡',});
-                const body = {id, updatedLikes, user}
-                await fetch(`${BASE_URL}/api/meme/likeandunlike`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body),
-                })
-            }catch(err){
-                console.error(err)
-            }
-        }
+    if (!liked) {
+      const updatedLikes = likes + 1
+      setLiked(true)
+      setLikes(updatedLikes)
+      try {
+        toast('Liked', { icon: '🧡' })
+        const body = { id, updatedLikes }
+        // await axios.post(`${BASE_URL}/api/meme/like`, {
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify(body)
+        // })
+        await fetch(`${BASE_URL}/api/meme/like`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        })
+      } catch (err) {
+        console.error(err)
+      }
     }
+    if (liked) {
+      const updatedLikes = likes - 1
+      setLiked(false)
+      setLikes(updatedLikes)
+      try {
+        toast('UnLiked', { icon: '💔' })
+        const body = { id, updatedLikes }
+        await fetch(`${BASE_URL}/api/meme/like`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+
+  async function handleLikeandUnlike() {
+    if (!session) {
+      toast.error('Please login to like Memes')
+      return
+    }
+    if (liked) {
+      const updatedLikes = likes - 1
+      setLiked(false)
+      setLikes(updatedLikes)
+      try {
+        toast('UnLiked', { icon: '💔' })
+        const body = { id, updatedLikes, user }
+        await fetch(`${BASE_URL}/api/meme/likeandunlike`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    if (!liked) {
+      const updatedLikes = likes + 1
+      setLiked(true)
+      setLikes(updatedLikes)
+      try {
+        toast('Liked', { icon: '🧡' })
+        const body = { id, updatedLikes, user }
+        await fetch(`${BASE_URL}/api/meme/likeandunlike`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
   return (
     <>
-        <div className='flex justify-between'>
-            <button onClick={handleLike} className="focus:outline-none">
-                {liked ?  <RiHeartFill size={25} color={`#ffa31a`}/> : <RiHeartAddLine size={25} color={`#ffa31a`}/>} 
-            </button> 
-            {likes === 0 ? <h1 className='pt-1 pl-5'> No Likes</h1>
-            : 
-            <h1 className='pt-1 pl-5'> {likes} {likes < 10 ? 'Like' : 'Likes'}</h1>
-            }
-        </div>
+      <div className="flex justify-between">
+        <button onClick={handleLike} className="focus:outline-none">
+          {liked ? (
+            <RiHeartFill size={25} color={`#ffa31a`} />
+          ) : (
+            <RiHeartAddLine size={25} color={`#ffa31a`} />
+          )}
+        </button>
+        {likes === 0 ? (
+          <h1 className="pt-1 pl-5"> No Like</h1>
+        ) : (
+          <h1 className="pt-1 pl-5">
+            {' '}
+            {likes} {likes < 10 ? 'Like' : 'Likes'}
+          </h1>
+        )}
+      </div>
     </>
   )
 }
--- src/pages/u/meme/[id].tsx
+++ src/pages/u/meme/[id].tsx
@@ -7,77 +7,91 @@ import LikeSection from '../../../components/LikeSection'
 import Link from 'next/link'
 import { NextSeo } from 'next-seo'
 import { MemeResponse } from '../../../utils/meme'
-import { useSession } from 'next-auth/react'
 
-
-const Meme = ({meme}: {meme:MemeResponse}) => {
-
-    const {data: session} = useSession()
-    
+const Meme = ({ meme }: { meme: MemeResponse }) => {
   return (
     <>
-        <NextSeo title={`Memehub`} description={`One Stop For All of Your Meme Needs`} openGraph={{
-            title: `Memehub`,
-            description: `One Stop For All of Your Meme Needs`,
-            url: `https://memehub.vercel.app/`,
-            images: [
-                {url : `${cloudinary}${meme.imageUrl}.png`}
-            ],
-            site_name: `Memehub`
-        }} 
-        />
-        <div className='flex flex-col justify-between items-center p-10'>
-                <h1 className='text-center text-2xl font-sans sm:text-4xl pb-5'>
-                    Uploaded By:
-                    <br />
-                    <div className='flex justify-between'>
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar pr-2">
-                            <div className="w-10 rounded-full">
-                                <Image src={`${meme.author.image}`} alt={`${meme.author.name}`} height={50} width={50} />
-                            </div>
-                        </label>
-                        <Link href={`/u/${meme.author.name}`}>
-                            <a className='relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] hover:before:scale-y-100 before:transition-transform before:ease-in-out before:duration-500 hover:cursor-pointer'>
-                                <span className='relative font-thin text-4xl'>{meme.author.name}</span>
-                            </a>
-                        </Link>
-                    </div>
-                </h1>
+      <NextSeo
+        title={`Memehub`}
+        description={`One Stop For All of Your Meme Needs`}
+        openGraph={{
+          title: `Memehub`,
+          description: `One Stop For All of Your Meme Needs`,
+          url: `https://memehub.vercel.app/`,
+          images: [{ url: `${cloudinary}${meme.imageUrl}.png` }],
+          site_name: `Memehub`,
+        }}
+      />
+      <div className="flex flex-col items-center justify-between p-10">
+        <h1 className="pb-5 text-center font-sans text-2xl sm:text-4xl">
+          Uploaded By:
+          <br />
+          <div className="flex justify-between">
+            <label
+              tabIndex={0}
+              className="avatar btn btn-ghost btn-circle pr-2"
+            >
+              <div className="w-10 rounded-full">
                 <Image
-                    className={`object-contain w-full h-96 ${meme.nsfw ? 'blur-lg' : ''} hover:blur-none`}
-                    src={`${cloudinary}${meme.imageUrl}.png`}
-                    alt="meme"
-                    height={450}
-                    width={500}
-                  />
-                <div className='pt-3'>
-                    <LikeSection likes={meme.likes}  id={meme.id} user={session.user} meme={meme}  />
-                </div>
+                  src={`${meme.author.image}`}
+                  alt={`${meme.author.name}`}
+                  height={50}
+                  width={50}
+                />
+              </div>
+            </label>
+            <Link href={`/u/${meme.author.name}`}>
+              <a className="relative before:absolute before:bottom-0 before:left-0 before:h-full before:w-full before:origin-bottom before:scale-y-[0.35] before:bg-sky-600 before:transition-transform before:duration-500 before:ease-in-out hover:cursor-pointer hover:before:scale-y-100">
+                <span className="relative text-4xl font-thin">
+                  {meme.author.name}
+                </span>
+              </a>
+            </Link>
+          </div>
+        </h1>
+        <Image
+          className={`h-96 w-full object-contain ${
+            meme.nsfw ? 'blur-lg' : ''
+          } hover:blur-none`}
+          src={`${cloudinary}${meme.imageUrl}.png`}
+          alt="meme"
+          height={450}
+          width={500}
+        />
+        <div className="pt-3">
+          <LikeSection
+            likes={meme.likes}
+            id={meme.id}
+            user={meme.author}
+            meme={meme}
+          />
         </div>
+      </div>
     </>
   )
 }
 
-export const getServerSideProps : GetServerSideProps = async(context) => {
-    const id = context.query.id
-    const meme =  await prisma.meme.findUnique({
-        where: {
-            // @ts-ignore
-            id: id
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const id = context.query.id
+  const meme = await prisma.meme.findUnique({
+    where: {
+      // @ts-ignore
+      id: id,
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+          image: true,
         },
-        include:{
-        author:{
-            select: {
-            name: true, image: true
-            }
-        }
-    }
-    })
-    return {
-        props: {
-            meme: JSON.parse(JSON.stringify(meme))
-        }
-    }
+      },
+    },
+  })
+  return {
+    props: {
+      meme: JSON.parse(JSON.stringify(meme)),
+    },
+  }
 }
 
 export default Meme
