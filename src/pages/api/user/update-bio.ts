6edead931f6ff6f1b4dba5969edba2c7c88d11ee
default-sameer <sameerjoshicr7@gmail.com>
2023-10-27 12:14:31 +0545

    Fix: Update Bio Api

--- src/pages/api/user/update-bio.ts
+++ src/pages/api/user/update-bio.ts
@@ -1,25 +1,27 @@
-import { getSession } from "next-auth/react"
-import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "../../../lib/prisma"
+import { getSession } from 'next-auth/react'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
-    const session = await getSession({ req })
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req })
 
-    if (session) {
-        const {updateBio} = req.body
-        const UserBio = await prisma.user.update({
-            where: {
-                name: session.user.name
-            },
-            data:{
-                bio: updateBio
-            }
-        })
-        res.json(UserBio)
-        
-    } else {
-        res.send({
-            error: "You must be signed in to MemeHub to access this API.",
-        })
-    }
+  if (session) {
+    const { updateBio } = req.body
+    const UserBio = await prisma.user.update({
+      where: {
+        email: session.user.email,
+      },
+      data: {
+        bio: updateBio,
+      },
+    })
+    res.json(UserBio)
+  } else {
+    res.send({
+      error: 'You must be signed in to MemeHub to access this API.',
+    })
+  }
 }
